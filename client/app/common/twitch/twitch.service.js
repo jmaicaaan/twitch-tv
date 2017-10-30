26dbba9f49e26012a75184c2d59bf6d19382a8bb
jmaicaaan <jmsantosss29@yahoo.com>
2017-10-31 00:53:38 +0800

    Closes #1, #2, and #3

--- client/app/common/twitch/twitch.service.js
+++ client/app/common/twitch/twitch.service.js
@@ -1,6 +1,56 @@
 class TwitchService {
-  constructor() {
+  constructor($http, $q) {
     "ngInject";
+    this._$http = $http;
+    this._$q = $q;
+    this.baseURL = 'https://wind-bow.glitch.me/twitch-api/';
+  };
+
+  getChannels = (name) => {
+    let channels = ['ESL_SC2', 'OgamingSC2', 'cretetion', 'freecodecamp', 'habathcx', 'RobotCaleb', 'noobs2ninjas'];
+    if (name) {
+      return channels.filter((channel) => channel.toLowerCase().indexOf(name) > - 1);
+    }
+    return channels;
+  };
+
+  retrieveChannelStatus = (channel) => {
+    if (!channel) {
+      return;
+    }
+    return this._$http.get(this.baseURL + 'channels/' + channel)
+      .then((resp) => resp.data)
+      .then((channel) => {
+        return this.checkForChannelStreams(channel.name)
+          .then((stream) => {
+            channel.stream = stream;
+            return channel;
+          });
+      })
+      .then((data) => data);
+  };
+
+  checkForChannelStreams = (channel) => {
+    if (!channel) {
+      return;
+    }
+    return this._$http.get(this.baseURL + 'streams/' + channel)
+      .then((resp) => resp.data);
+  };
+
+  retrieveAllStatus = () => {
+    let channels = this.getChannels();
+    let promises = [];
+    let defer = this._$q.defer();
+
+    angular.forEach(channels, (channel) => {
+      let promise = this.retrieveChannelStatus(channel);
+      promises.push(promise);
+    });
+
+    this._$q.all(promises)
+      .then((response) => defer.resolve(response));
+    return defer.promise;
   };
 }
 
--- client/app/components/home/home.controller.js
+++ client/app/components/home/home.controller.js
@@ -1,12 +1,34 @@
+const dialog = require('../dialog/dialog.pug');
+
 class HomeController {
-  constructor(twitchService) {
+  constructor(twitchService, $mdDialog) {
     "ngInject";
     this.text = 'Hola Amigo';
     this._twitchService = twitchService;
+    this._$mdDialog = $mdDialog;
+    this.results = [];
   };
   
   $onInit = () => {
-    console.log('this._twitchService', this._twitchService);
+    this._twitchService.retrieveAllStatus()
+      .then((results) => {
+        console.log('results', results);
+        this.results = results;
+      });
+  };
+
+  viewDetails = (channel) => {
+    console.log('channel', channel);
+    this._$mdDialog.show({
+      template: dialog,
+      parent: angular.element(document.body),
+      locals: { channel },
+      clickOutsideToClose: true,
+      controller: ($scope, channel) => {
+        "ngInject";
+        $scope.channel = channel;
+      }
+    });
   };
 }
 
